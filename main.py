761aac30b4811d869044019e99951a381da70207
Liore-S <goober395@gmail.com>
2023-03-14 10:19:02 +0700

    add: color coded text

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import configparser
+# import configparser
 from serpapi import GoogleSearch
 import datetime
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-api_keys = config['API_KEYS']['MY_API_KEY']
+# config = configparser.ConfigParser()
+# config.read('config.ini')
+# api_keys = config['API_KEYS']['MY_API_KEY']
 
 params = {
     "engine": "google",
     "q": "Jadwal sholat di semarang hari ini",
     "hl": "en",
     "gl": "id",
-    "api_key": api_keys
+    "api_key": 'c57e779ec89c6d6c54a88bb1a4374648d0aec15d855fd2b6f2c71c3a8d22b7fa'
 }
 
 search = GoogleSearch(params)
@@ -34,25 +34,26 @@ isya = datetime.datetime.strptime(isya, '%H.%M').strftime('%H:%M')
 
 # get the current time
 now = datetime.datetime.now()
-print("Waktu Saat Ini \n" + now.strftime("%H:%M")+"\n")
+current_time = now.strftime("%H:%M")
+print(f"Waktu Saat Ini\n\033[91m{current_time}\033[0m")
 
 # create case to check the time
 if now.strftime("%H:%M") < subuh:
     print("\nJadwal sholat mendatang ")
 if now.strftime("%H:%M") < subuh:
-    print(f"Subuh \n{subuh}\n")
+    print(f"\033[33mSubuh \n{subuh}\033[0m\n")
 elif now.strftime("%H:%M") < dzuhur:
     print("\nJadwal sholat mendatang ")
-    print(f"Dzuhur \n{dzuhur}\n")
+    print(f"\033[33mDzuhur \n{dzuhur}\033[0m\n")
 elif now.strftime("%H:%M") < ashar:
     print("\nJadwal sholat mendatang ")
-    print(f"Ashar \n{ashar}\n")
+    print(f"\033[33mAshar \n{ashar}\033[0m\n")
 elif now.strftime("%H:%M") < maghrib:
     print("\nJadwal sholat mendatang ")
-    print(f"Maghrib \n{maghrib}\n")
+    print(f"\033[33mMaghrib \n{maghrib}\033[0m\n")
 elif now.strftime("%H:%M") < isya:
     print("\nJadwal sholat mendatang ")
-    print(f"Isya \n{isya}\n")
+    print(f"\033[33mIsya \n{isya}\033[0m\n")
 
 
 print("Jadwal Sholat Hari Ini")
